1019b72ca531184b1d849c6bf81cb00f2228c1b1
Leila Abdi <leila.abdi@student.moringaschool.com>
2023-12-10 17:09:22 +0300

    comleted assigment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+// index.js
+const DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const left = positionToInteger(DODGER.style.left);
+
+    if (left > 0) {
+        DODGER.style.left = `${left - 4}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const left = positionToInteger(DODGER.style.left);
+
+    if (left < 360) {
+        DODGER.style.left = `${left + 4}px`;
+    }
+}
+
+function positionToInteger(position) {
+    return parseInt(position.split('px')[0]) || 0;
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
+
